6b03f02647da3345e3ed5e9d344c2fc84fe30a31
OrexAn <olenivse3@gmail.com>
2022-04-17 09:46:05 +0300

    file updates

---
+++ src/main/webapp/resources/js/share.js
@@ -0,0 +1,24 @@
+$(window).on('load',function(){
+    $('#myTrigger').click(function (){
+        $('#myModal').modal('show');
+    });
+});
+$(document).ready(function(){
+    var questionnaireId = $("[name='questionnaireId']").first().val();
+    $('#generatedLinkFieldId').val(window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/questionnaire/" + questionnaireId);
+});
+
+function goToEditor(){
+    var questionnaireId = $("[name='questionnaireId']").first().val();
+    window.location.replace("questionnaire/builder/" + questionnaireId);
+}
+
+function toSharePage(){
+    var questionnaireId = $("[name='questionnaireId']").first().val();
+    window.location.replace("questionnaire/share/" + questionnaireId);
+}
+
+function toResultsPage(){
+    var questionnaireId = $("[name='questionnaireId']").first().val();
+    window.location.replace("/questionnaire/" + "results/" + questionnaireId);
+}
